3e06ad883a768c00809b21f4a6bf7a763e05ff90
alexandreakdeniz <alexandre.akdeniz@free.fr>
2023-08-20 23:22:41 +1200

    Able to consume a message

--- Application/Services/KafkaLogConsumerService.cs
+++ Application/Services/KafkaLogConsumerService.cs
@@ -1,19 +1,25 @@
+using System.Text;
+using Application.Common.Interfaces;
 using Confluent.Kafka;
+using Domain.Entities.LogEntities;
 using FappCommon.Kafka.Config;
+using FappCommon.Kafka.Log;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using System.Text.Json;
 
 namespace Application.Services;
 
 public class KafkaLogConsumerService : BackgroundService
 {
-    private readonly ILogger<KafkaLogConsumerService> _logger;
     private readonly KafkaConsumerConfig _config;
+    private readonly IServiceScopeFactory _scopeFactory;
 
-    public KafkaLogConsumerService(KafkaConsumerConfig config, ILogger<KafkaLogConsumerService> logger)
+    public KafkaLogConsumerService(IServiceScopeFactory scopeFactory, KafkaConsumerConfig config)
     {
+        _scopeFactory = scopeFactory;
         _config = config;
-        _logger = logger;
     }
 
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
@@ -25,26 +31,59 @@ public class KafkaLogConsumerService : BackgroundService
 #pragma warning restore CA2016
     }
 
-    private void Perform(CancellationToken stoppingToken)
+    private async Task Perform(CancellationToken stoppingToken)
     {
-        using IConsumer<Ignore, string>? consumer = new ConsumerBuilder<Ignore, string>(_config.ConsumerConfig).Build();
+        IServiceScope scope = _scopeFactory.CreateScope();
+        using IApplicationDbContext context = scope.ServiceProvider.GetRequiredService<IApplicationDbContext>();
+        
+        JsonDeserializer<KafkaLogMessage> deserializer = new JsonDeserializer<KafkaLogMessage>();
+        using IConsumer<Ignore, KafkaLogMessage>? consumer = new ConsumerBuilder<Ignore, KafkaLogMessage>(_config.ConsumerConfig)
+            .SetValueDeserializer(deserializer)
+            .Build();
         consumer.Subscribe(_config.Topic);
-
+        
         while (!stoppingToken.IsCancellationRequested)
         {
             try
             {
-                ConsumeResult<Ignore, string>? consumeResult = consumer.Consume(stoppingToken);
-                _logger.LogInformation("Consumed message: {MessageValue}", consumeResult.Message.Value);
+                ConsumeResult<Ignore, KafkaLogMessage>? consumeResult = consumer.Consume(stoppingToken);
+                if (consumeResult is null)
+                    continue;
+                
+                KafkaLogMessage message = consumeResult.Message.Value;
+
+                Log log = new Log
+                {
+                    Template = message.Template,
+                    Level = message.LogLevel,
+                    Timestamp = message.Timespan,
+                    Data = BsonDocument.Parse(message.Data)
+                };
+                
+                await context.Logs.InsertOneAsync(log, cancellationToken: stoppingToken);
             }
             catch (OperationCanceledException)
             {
                 // Graceful shutdown
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                _logger.LogError(ex, "Error consuming message");
+                // ignore
             }
         }
+    }
+}
+
+public class JsonDeserializer<T> : IDeserializer<T>
+{
+    public T Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+    {
+        if (isNull)
+        {
+            return default!;
+        }
+
+        string json = Encoding.UTF8.GetString(data.ToArray());
+        return JsonSerializer.Deserialize<T>(json)!;
     }
 }
